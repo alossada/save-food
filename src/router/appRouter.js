abd1375bc685da5811ebf6b6fea535930f100b73
unknown <alossada@gmail.com>
2022-07-30 12:19:13 -0500

    Router

---
+++ src/router/appRouter.js
@@ -0,0 +1,23 @@
+import {Routes, Route} from 'react-router-dom'
+import Login from '../components/Login/Login'
+import Register from '../components/Register/Register'
+import Board from '../components/Board/Board'
+import AuthProvider from '../context/authContext'
+import { ProtectedRoute } from './protectedRoute'
+
+export default function AppRouter() {
+    return (
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/board' 
+          element={
+            <ProtectedRoute>
+              <Board/>
+            </ProtectedRoute>
+        }/>
+      </Routes>
+    </AuthProvider>
+  )
+}
