4d345cc7174261ee92c5e2e801662304d96ced5d
unknown <alossada@gmail.com>
2022-07-31 11:33:19 -0500

    Se organiza ruteo de las categorias

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,28 +1,45 @@
 import { useAuth } from '../../context/authContext'
-//import Traking from './Traking';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Board() { 
 
   const {user, logout, loading} =useAuth()//me trae la información del usuario (verifica el login)  
   console.log(user);
+  
+  const navigate = useNavigate();
 
   const handleLogout = async() => {
     await logout();    
   }
 
-  if(loading) return <h1>loading</h1>
+  if(loading) return <h1>Loading</h1>
+
+   // función cambio de ruta a Categoria1
+  const handleChangeCategory1 = () => {
+    return navigate('/board/fruitvegetable', { replace: true });
+  };
+   // función cambio de ruta a Categoria2
+  const handleChangeCategory2 = () => {
+    return navigate('/board/protein', { replace: true });
+  };
+  // función cambio de ruta a Categoria3
+  const handleChangeCategory3 = () => {
+    return navigate('/board/carbohydrate', { replace: true });
+  };
+  // función cambio de ruta a Categoria4
+  const handleChangeCategory4 = () => {
+    return navigate('/board/finishfood', { replace: true });
+  };
 
   return (
     <div>
-      <button onClick={handleLogout}>
-        logout
-      </button>
+      <button onClick={handleLogout}>CERRAR SESIÓN</button>
       <p>Elije la categoría de los desechos que deseas registrar</p>
-      <button>Frutas y verduras</button>
-      <button>Proteinas</button>
-      <button>Carbohidratos</button>
-      <button>Comida preparada</button>
+      <button onClick={handleChangeCategory1}>Frutas y verduras</button>
+      <button onClick={handleChangeCategory2}>Proteinas</button>
+      <button onClick={handleChangeCategory3}>Carbohidratos</button>
+      <button onClick={handleChangeCategory4}>Comida preparada</button>
     </div>   
   )
 }
